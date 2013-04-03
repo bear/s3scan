a41f67a42021dde1917bca4dfc3b825581d33184
bear (Mike Taylor) <bear@andyet.net>
2013-04-02 22:34:48 -0400

    added command line options which will override config file added format option for text or csv (default is text) split code up into functions to facilitate format option

--- s3scan.py
+++ s3scan.py
@@ -4,14 +4,24 @@
 
 Usage:
 
-  python s3scan.py
+  python s3scan.py [-k <api_key>] [-s <api_secret>] [-f <format>]
 
-  No parameters or options right now, just create an s3scan.cfg
-  with the following:
+Options:
+
+  -k | --key     S3 Access Key
+  -s | --secret  S3 Access Secret
+  -f | --format  Output format
+                 [Optional]
+
+  Where the output format can be either 'text' or 'csv'
+
+If no paramters are given, the configuration file 's3scan.cfg' will
+be loaded and should contain:
 
     [aws]
     access_key = YOUR_KEY_HERE
     access_secret = YOUR_SECRETS_HERE
+    format = text
 
 """
 
@@ -28,65 +38,112 @@ __version__   = u'.'.join(map(str, VERSION[0:3])) + u''.join(VERSION[3:])
 import os
 import ConfigParser
 
+from optparse import OptionParser
 from xml.etree import cElementTree as ET
 
 from boto.s3.connection import S3Connection
 
-config = ConfigParser.SafeConfigParser()
-config.read('s3scan.cfg')
 
-api_key    = config.get('aws', 'access_key')
-api_secret = config.get('aws', 'access_secret')
+def getConfig():
+    parser = OptionParser()
 
-c = S3Connection(api_key, api_secret)
+    parser.add_option('-k', '--key',    dest='api_key',    default='',     help='S3 Access Key')
+    parser.add_option('-s', '--secret', dest='api_secret', default='',     help='S3 Access Secret')
+    parser.add_option('-f', '--format', dest='format',     default='text', help='Output format: text, csv')
 
-rs = c.get_all_buckets()
+    options, args = parser.parse_args()
 
-print 'Scanning', len(rs), 'buckets'
+    api_key    = options.api_key
+    api_secret = options.api_secret
+    format     = options.format
 
-buckets = {}
-maxName = 0
+    if os.path.isfile('s3scan.cfg'):
+        config = ConfigParser.SafeConfigParser({ 'access_key':    api_key,
+                                                 'access_secret': api_secret,
+                                                 'format':        format,
+                                               })
+        config.read('s3scan.cfg')
 
-for o in rs:
-    buckets[o.name] = {}
+        api_key    = config.get('aws', 'access_key')
+        api_secret = config.get('aws', 'access_secret')
+        format     = config.get('aws', 'format').lower()
 
-    if len(o.name) > maxName:
-        maxName = len(o.name)
+    return (api_key, api_secret, format)
 
-    acp = o.get_acl()
-    xml = acp.acl.to_xml()
+def discoverBuckets(api_key, api_secret):
+    c  = S3Connection(api_key, api_secret)
+    rs = c.get_all_buckets()
 
-    xRoot = ET.fromstring(xml)
-    for grant in xRoot:
-        grantee = { 'id': None, 'name': None, 'permission': [] }
+    buckets = {}
+    maxName = 0
 
-        for item in grant:
-            if item.tag == 'Grantee':
-                for element in item:
-                    if element.tag == 'ID':
-                        grantee['id'] = element.text
-                    elif element.tag == 'DisplayName':
-                        grantee['name'] = element.text
-                    elif element.tag == 'URI':
-                        grantee['id']   = element.text
-                        grantee['name'] = element.text.split('/')[-1]
-            elif item.tag == 'Permission':
-                grantee['permission'].append(item.text)
+    for b in rs:
+        bucketName          = b.name
+        buckets[bucketName] = {}
 
-        if grantee['name'] not in buckets[o.name]:
-            buckets[o.name][grantee['name']] = []
+        if len(bucketName) > maxName:
+            maxName = len(bucketName)
 
-        buckets[o.name][grantee['name']].append((grantee['id'], grantee['permission']))
+        acp = b.get_acl()
+        xml = acp.acl.to_xml()
 
-for key in buckets:
-    bucket     = buckets[key]
+        xRoot = ET.fromstring(xml)
+        for grant in xRoot:
+            grantee = { 'id': None, 'name': None, 'permission': [] }
+
+            for item in grant:
+                if item.tag == 'Grantee':
+                    for element in item:
+                        if element.tag == 'ID':
+                            grantee['id'] = element.text
+                        elif element.tag == 'DisplayName':
+                            grantee['name'] = element.text
+                        elif element.tag == 'URI':
+                            grantee['id']   = element.text
+                            grantee['name'] = element.text.split('/')[-1]
+                elif item.tag == 'Permission':
+                    grantee['permission'].append(item.text)
+
+            if grantee['name'] not in buckets[bucketName]:
+                buckets[bucketName][grantee['name']] = []
+
+            buckets[bucketName][grantee['name']].append((grantee['id'], grantee['permission']))
+
+    return buckets, maxName
+
+def csvFormat(bucket):
     reads      = []
     writes     = []
     reads_acp  = []
     writes_acp = []
 
     for grantee in bucket:
-        s = ''
+        for grantee_id, permission in bucket[grantee]:
+            if 'READ' in permission:
+                reads.append(grantee)
+            if 'WRITE' in permission:
+                writes.append(grantee)
+            if 'READ_ACP' in permission:
+                reads_acp.append(grantee)
+            if 'WRITE_ACP' in permission:
+                writes_acp.append(grantee)
+
+    l = [key,
+         ';'.join(writes),
+         ';'.join(reads),
+         ';'.join(writes_acp),
+         ';'.join(reads_acp),
+        ]
+
+    return ','.join(l)
+
+def textFormat(bucket, maxName):
+    reads      = []
+    writes     = []
+    reads_acp  = []
+    writes_acp = []
+
+    for grantee in bucket:
         for grantee_id, permission in bucket[grantee]:
             if 'READ' in permission:
                 reads.append(grantee)
@@ -108,4 +165,17 @@ for key in buckets:
     if len(reads_acp) > 0:
         s += t + 'ACP Read: %s' % ','.join(reads_acp)
 
-    print s
+    return s
+
+if __name__ == '__main__':
+    api_key, api_secret, format = getConfig()
+
+    buckets, maxName = discoverBuckets(api_key, api_secret)
+
+    for key in buckets:
+        bucket = buckets[key]
+
+        if format == 'csv':
+            print csvFormat(bucket)
+        else:
+            print textFormat(bucket, maxName)
